76976fb5e2bc78f3102546fded612dc0501e438a
EpsilonFO <102187510+EpsilonFO@users.noreply.github.com>
2025-06-18 16:44:07 +0200

    Delete mov_to_mp4.py

--- mov_to_mp4.py
+++
@@ -1,7 +0,0 @@
-from moviepy import VideoFileClip
-
-# Charger la vidéo .mov
-clip = VideoFileClip("pics/felix_vid.mov")
-
-# Exporter en .mp4
-clip.write_videofile("pics/felix.mp4", codec="libx264")
